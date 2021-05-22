6167be780c914f8f04da13b7f4869349da0cf11a
Kenneth Kim <kenneth@Kenneths-MacBook-Air.local>
2021-05-22 15:17:27 +0900

    first commit

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+// custom typefaces
+import "typeface-spoqa-han-sans2"
+import "typeface-manrope"
+import "typeface-ibm-plex-sans"
+// normalize CSS across browsers
+import "./src/normalize.css"
+// custom CSS styles
+import "./src/style.css"
+
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
